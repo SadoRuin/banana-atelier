cf607b39e1565cc0a04e9ae1c25d959c26a31651
박윤환 <dope2514@gmail.com>
2023-02-02 15:16:38 +0900

    :bug: [BE] 실행가능하도록 살짝 수정

--- Backend/src/main/java/com/ssafy/banana/api/controller/CurationController.java
+++ Backend/src/main/java/com/ssafy/banana/api/controller/CurationController.java
@@ -3,7 +3,7 @@ package com.ssafy.banana.api.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,21 +14,21 @@ import com.ssafy.banana.dto.CurationDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @Api(tags = "큐레이션 관련 API")
 @RequestMapping("/curations")
+@RequiredArgsConstructor
 public class CurationController {
 
-	private final CurationService curationService = null;
+	private final CurationService curationService;
 
 	@GetMapping("/list")
 	@ApiOperation(value = "큐레이션 리스트")
 	public ResponseEntity<CurationDto> signup(
 		@ApiParam(value = "email, password, nickname") @Valid @RequestBody CurationDto curationDto) {
-		return ResponseEntity.ok(userService.signup(userDto));
+		return ResponseEntity.ok().build();
 	}
-
-
 
 }
--- Backend/src/main/java/com/ssafy/banana/api/service/CurationService.java
+++ Backend/src/main/java/com/ssafy/banana/api/service/CurationService.java
@@ -1,4 +1,10 @@
 package com.ssafy.banana.api.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CurationService {
 }
