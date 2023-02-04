a46834b2625f97c0c4e4b030261f26620477aa16
Park Yunhwan <dope2514@gmail.com>
2023-02-05 00:04:25 +0900

    :white_check_mark: [FE] 배포환경에서 back과 연동 테스트

--- Frontend/src/_actions/user_action.js
+++ Frontend/src/_actions/user_action.js
@@ -10,7 +10,7 @@ import {
 export function loginUser(dataTosubmit) {
 
     // const request = axios.post('https://i8a108.p.ssafy.io/api/users/login', dataTosubmit)
-    const request = axios.post('http://localhost:8099/auth/login', dataTosubmit)
+    const request = axios.post('http://back:8099/api/auth/login', dataTosubmit)
         .then(response => response)
         .catch(error => console.log('error', error))
     console.log(request);
