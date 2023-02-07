63d0a7239a80c8ee203927a507a6408113e5e624
gyur1kim <qri1114@gmail.com>
2023-02-07 15:00:22 +0900

    :bug: [FE] height, weight 에러수정
    - react는 height=100%같은 값이 적용되지 않는다
    - Link안에 Link 중첩이 불가능하다

--- Frontend/src/components/commons/artsThumbnail.js
+++ Frontend/src/components/commons/artsThumbnail.js
@@ -5,12 +5,12 @@ import './artsThumbnail.css'
 function ArtsThumbnail({art_hit, art_like_count, art_name, art_seq, art_thumbnail, nickname, user_seq}) {
   return (
     <div className="art-thumbnail__container">
-      <div className="art-thumbnail">
-        <div className="hidden" width="100%" height="100%">{art_name}</div>
-        <img src="https://item.kakaocdn.net/do/2697398bad5927125f3db2f8a248ca15f604e7b0e6900f9ac53a43965300eb9a" height alt=""/>
+      <div className="art-thumbnail" >
+        <div className="hidden">{art_name}</div>
+        <img src="https://item.kakaocdn.net/do/2697398bad5927125f3db2f8a248ca15f604e7b0e6900f9ac53a43965300eb9a" alt=""/>
       </div>
       <div>
-        <Link className="Link" to={`../${nickname}`}>{nickname}작가 </Link>
+        <div>{nickname}작가 </div>
         <div>조회수{art_hit}</div>
         <div>좋아요{art_like_count}</div>
       </div>
