dbe3a47b5e44720b06d83d679a8ad007fd43789d
gyur1kim <qri1114@gmail.com>
2023-02-08 16:54:21 +0900

    :lipstick: [FE] 좋아요 버튼 생성
    - fontawesome을 이용해 좋아요 버튼을 만들었다
    - 아직 유저가 좋아요를 했는지 여부에 따라 빈하트, 찬 하트를 하는 것은 미구현
    - 작품 컴포넌트에서 hover를 해야 좋아요 버튼이 생기는 것도 미구현

--- Frontend/src/routes/ArtsPage/ArtsDetail.js
+++ Frontend/src/routes/ArtsPage/ArtsDetail.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import {Link, useLoaderData} from 'react-router-dom'
-import ProfileImg from "../../components/commons/ProfileImg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHearEmpty } from '@fortawesome/free-regular-svg-icons'
+
 import customAxios from '../../_actions/customAxios';
+import ProfileImg from "../../components/commons/ProfileImg";
+import {YellowBtn , LikeBtn} from "../../components/commons/buttons";
+import './ArtsDetail.css'
 
 export async function loader ({params}) {
   let artSeq = params.art_seq;
@@ -23,7 +29,7 @@ function ArtsDetail() {
   const artData = useLoaderData();
 
   return (
-    <div>
+    <div className="content__container">
 
       <div className="art-detail__container">
         {/* 작품 사진 */}
@@ -33,7 +39,7 @@ function ArtsDetail() {
         <div className="art-detail_content">
           <div>
             <h1>{artData.art_name}</h1>
-            <Link className="artist_profile Link" to={`./${artData.nickname}/arts`}>
+            <Link className="artist_profile Link" to={`../${artData.nickname}/arts`}>
               {/* 프로필 이미지는 아직입니다!! src={artData.profile_img}*/}
               <ProfileImg height="30px" width="30px" />
               <div>{artData.nickname} <span className="jakka">작가</span></div>
@@ -50,12 +56,11 @@ function ArtsDetail() {
             <div>
               <div className="views">
                 <img src="ArtsMain" alt="" />
-                viewers:
-                {artData.art_hit}
+                조회수 : {artData.art_hit}
               </div>
               <div className="downloaded">
                 <img src="ArtsMain" alt="" />
-                downloaded:이 값이 없다!
+                다운로드 : {artData.art_download_count}
               </div>
               <div className="likes">
                 <img src="ArtsMain" alt="" />
@@ -63,8 +68,11 @@ function ArtsDetail() {
               </div>
             </div>
             <div>
-              <button>좋아요</button>
-              <button>다운로드</button>
+              {/* 좋아요 누른 버튼이랑 안누른 버튼 */}
+              <LikeBtn><FontAwesomeIcon icon={faHeart} /></LikeBtn>
+              <LikeBtn><FontAwesomeIcon icon={faHearEmpty} /></LikeBtn>
+              {/* 이미지 다운로드는 아직인듯? */}
+              <YellowBtn>다운로드</YellowBtn>
             </div>
           </div>
         </div>
