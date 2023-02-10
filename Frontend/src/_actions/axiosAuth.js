90e8f4007d765150d1b952c679b691d37b7a3131
shpark0913 <shpark0913@naver.com>
2023-02-10 13:51:00 +0900

    :bug: [FE] axios 인스턴스 재생성
    - 기존의 customAxios()에서 많은 오류 발생
    - axiosAuth와 axiosCustom 이라는 2가지 인스턴스 만들어서 해결
      - header에 토큰 보내야 한다면 axiosAuth 사용
      - header에 토큰 필요치 않다면 axiosCustorm 사용

---
+++ Frontend/src/_actions/axiosAuth.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+let expiration = +localStorage.getItem("expiration")
+let nowTime = new Date()
+let nowTime_sec = nowTime.getTime()
+let diffTime = (expiration - nowTime_sec)/1000
+
+
+if (diffTime < 30) {
+    axios.post('https://i8a108.p.ssafy.io/api/auth/reissue',
+    { headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+    })
+    .then(response => {
+        localStorage.setItem("token", response.data.token)
+        localStorage.setItem("expiration", response.data.expiration)
+    })
+}
+const Token = localStorage.getItem("token")
+
+const axiosAuth = axios.create({
+    baseURL: 'https://i8a108.p.ssafy.io/api/',
+    headers: {
+        'Authorization' : `Bearer ${Token}`
+    },
+})
+
+export default axiosAuth
