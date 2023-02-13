ed09e0df6406122a6d0cede0a64c25b60c65a015
gyur1kim <qri1114@gmail.com>
2023-02-13 10:36:47 +0900

    :sparkles: [FE] 공지사항 탭메뉴 적용
    - 내가 작가일 경우 공지 작성 버튼 렌더링
    - 작가 페이지인 경우 공지사항 렌더링
    - 내 페이지인 경우 팔로잉 작가들의 공지사항 렌더링

--- Frontend/src/routes/MyPage/Layout.js
+++ Frontend/src/routes/MyPage/Layout.js
@@ -3,6 +3,7 @@ import {Outlet, NavLink, Form, useLoaderData} from "react-router-dom";
 import { axiosAuth } from "../../_actions/axiosAuth";
 
 import ProfileImg from "../../components/commons/ProfileImg";
+import { YellowBtn } from "../../components/commons/buttons";
 import './Layout.css'
 
 export async function loader ({params}) {
@@ -22,6 +23,7 @@ export default function Layout() {
   const isMyPage = nickname === localStorage.getItem('nickname')
   const isArtist = userData.role === 'ROLE_ARTIST'
 
+
   return (
     // 얘는 Mypage의 layout임!!! 마이페이지 어디를 가든 변하지 않고 항상 있어야 함 (==사이드바랑 위의 메뉴탭 4개)
     <div className="grid_my-page-layout">
@@ -34,31 +36,33 @@ export default function Layout() {
         { isArtist ?
           <div id="sns_links">
             <div id="email">이메일 : {userData.email}</div>
-            <div id="instagram">인스타그램 : {userData.instagramLink}</div>
-            <div id="twitter">트위터 : {userData.twitterLink}</div>
-            <div id="youtube">블로그 : {userData.blogLink}</div>
-          </div> : null }
+            {userData.instagranLink? <div id="instagram">인스타그램 : {userData.instagramLink}</div> : null }
+            {userData.twitterLink? <div id="twitter">트위터 : {userData.twitterLink}</div> : null }
+            {userData.blogLink? <div id="blog">블로그 : {userData.blogLink}</div> : null }
+          </div> : null
+        }
 
-          {/* userSeq가 내가 아니면 남의 버튼 렌더링, 나라면 나의 버튼 렌더링 */}
-          { isMyPage ?
-            <div id="profile_buttons">
-              <Form action={'edit_profile'}><button type="submit">정보 수정하기</button></Form>
-              <Form action={'upload'}><button type="submit">작품 업로드</button></Form>
-            </div>
-            :
-            <div id="profile_buttons">
-              <button id='follow'>팔로우버튼</button>
-            </div>
-          }
+        {/* userSeq가 내가 아니면 남의 버튼 렌더링, 나라면 나의 버튼 렌더링 */}
+        { isMyPage ?
+          <div id="profile_buttons">
+            <Form action={'edit_profile'}><YellowBtn type="submit">정보 수정하기</YellowBtn></Form>
+            <Form action={'upload'}><YellowBtn type="submit">작품 업로드</YellowBtn></Form>
+          </div>
+          :
+          <div id="profile_buttons">
+            <YellowBtn id='follow'>팔로우버튼</YellowBtn>
+          </div>
+        }
       </div>
 
       <div className="my-page__content">
         <nav>
           <NavLink to='.' className={({isActive}) => isActive? 'link nav-active' : 'link' } end>작품</NavLink>
 
-          <NavLink to={ ( isMyPage && !isArtist) ? 'notices/following' : 'notices/mine'  } className={({isActive}) => isActive? 'link nav-active' : 'link' } >공지사항</NavLink>
+          {/* 공지사항은 내 페이지도 아니고 작가도 아니면 아예 링크를 띄우지 말자 */}
+          { (!isMyPage && !isArtist)? null : <NavLink to={ 'notices' } className={({isActive}) => isActive? 'link nav-active' : 'link' }>공지사항</NavLink>}
           <NavLink to={ ( isMyPage && !isArtist) ? 'curations/following' : 'curations/mine' } className={({isActive}) => isActive? 'link nav-active' : 'link' } >큐레이션</NavLink>
-          <NavLink to='commissions' className={({isActive}) => isActive? 'link nav-active' : 'link' } >커미션</NavLink>
+          {/*<NavLink to='commissions' className={({isActive}) => isActive? 'link nav-active' : 'link' } >커미션</NavLink>*/}
         </nav>
 
         {/* 여기가 진짜 렌더링 해야하는 곳인데..... */}
--- Frontend/src/routes/MyPage/NoticesRoot.js
+++ Frontend/src/routes/MyPage/NoticesRoot.js
@@ -1,33 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData } from "react-router-dom";
 import { axiosAuth, axiosReissue } from "../../_actions/axiosAuth";
-// import { useLoaderData, Form } from "react-router-dom";
+import {TabMenu, TabContent} from "../../components/commons/TabMenuComponent";
 
 export async function loader ({params}) {
   const [nickname, userSeq] = params.nickname_user_seq.split('@');
 
   axiosReissue();
+
+  // 나의 공지, 없으면 null
   const userNotices = await axiosAuth(`notices/${userSeq}`)
     .then(response => response.data)
     .catch(() => null)
+  // 팔로잉 한 작가들의 공지, 없으면 null
   const followingNotices = await axiosAuth(`notices/${userSeq}/following`)
     .then(response => response.data)
     .catch(() => null)
+  // 이 마이페이지 주인이 작가인지 알기위해 필요함
+  const userData = await axiosAuth.get(`users/profile/${userSeq}`)
+    .then(response => response.data)
+    .catch(error => console.log(error))
 
   console.log(userNotices);
   console.log(followingNotices);
-  return [nickname, userSeq, userNotices, followingNotices]
+  return [nickname, userSeq, userData, userNotices, followingNotices]
 }
 
 
 function NoticesRoot() {
-  // const [nickname, userSeq, userNotices, followingNotices] = useLoaderData();
-  // const [nonticeIndex, setNoticeIndex] = useState(0)
-  // const isMyPage = nickname === localStorage.getItem('nickname')
-  // const isArtist = userData.role === 'ROLE_ARTIST'
+  const [nickname, userSeq, userData, userNotices, followingNotices] = useLoaderData();
+  const [noticeIndex, setNoticeIndex] = useState(0)
+  const isMyPage = nickname === localStorage.getItem('nickname');
+  const isArtist = userData.role === "ROLE_ARTIST"
+
+  console.log(userSeq)
+  const noticesMenuData = [];
+  if (isArtist) {
+    noticesMenuData.push({
+        name: "공지사항",
+        content:
+        <div>
+          {isMyPage ? <div>공지사항 등록 버튼</div> : null}
+          {userNotices ?
+            userNotices.map((notice) => <div>{notice.noticeTitle}</div>)
+            :
+            <div>등록된 공지사항이 없습니다.</div>}
+        </div>
+      })
+  }
+  if (isMyPage) {
+    noticesMenuData.push({
+      name: "작가들 공지",
+      content: followingNotices?
+        followingNotices.map((notice) => <div>{notice.noticeTitle}</div> )
+        :
+        <div>팔로잉한 작가가 없습니다.</div>
+    })
+  }
+
 
   return (
     <div>
-
+      <div>
+        <TabMenu>
+          { noticesMenuData.map((tab, idx) =>
+            <li
+              className={idx === noticeIndex ? "submenu focused" : "submenu" }
+              onClick={ () => setNoticeIndex(idx) }
+            >
+              {tab.name}
+            </li>) }
+        </TabMenu>
+        <TabContent>
+          { noticesMenuData[noticeIndex].content }
+        </TabContent>
+      </div>
     </div>
   );
 }
