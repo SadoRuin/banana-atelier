292c024ed4ec791d60e80e24d660b492fd5e5a1e
gyur1kim <qri1114@gmail.com>
2023-02-15 00:24:05 +0900

    :recycle: [FE] 마이페이지 작품 탭메뉴 정리

--- Frontend/src/routes/MyPage/ArtsRoot.js
+++ Frontend/src/routes/MyPage/ArtsRoot.js
@@ -1,39 +1,59 @@
 import React, { useState } from 'react';
-import { useLoaderData, Link, redirect } from "react-router-dom";
+import { useLoaderData, Link, redirect, useOutletContext } from "react-router-dom";
+
 import { axiosAuth, axiosReissue } from "../../_actions/axiosAuth";
-import { TabMenu, TabContent } from "../../components/commons/TabMenuComponent";
+import TabMenuComponent from "../../components/commons/TabMenuComponent";
 import ArtItemMyPage from "../../components/MyPage/ArtItemMyPage";
-import {YellowBtn} from "../../components/commons/buttons";
+import { YellowBtn } from "../../components/commons/buttons";
 
 export async function loader ({params}) {
   const userSeq = params.nickname_user_seq.split('@')[1];
   axiosReissue();
 
-
-  const userArts = await axiosAuth(`arts`)
+  // 작가의 작품
+  const userArts = await axiosAuth(`arts/${userSeq}`)
     .then(response => response.data)
     .catch((error) => error.response.status === 401 ? error : null )
+  // 작가가 좋아요한 작품
   const userLikes = await axiosAuth(`arts/${userSeq}/like`)
     .then(response => response.data)
     .catch((error) => error.response.status === 401 ? error : null )
-  const userMasterpiece = await axiosAuth(`arts/${userSeq}/masterpiece`)
+  // 작가의 대표작품
+  const userMasterpieces = await axiosAuth(`arts/${userSeq}/masterpiece`)
     .then(response => response.data)
     .catch((error) => error.response.status === 401 ? error : null )
   console.log(userArts);
   console.log(userLikes);
-  console.log(userMasterpiece);
-  if (userArts?.response?.status === 401 || userLikes?.response?.status === 401 || userMasterpiece?.response?.status === 401) {
+  console.log(userMasterpieces);
+  // 401은 권한이 없다는 뜻이므로 login으로 보내버리자
+  if (userArts?.response?.status === 401 || userLikes?.response?.status === 401 || userMasterpieces?.response?.status === 401) {
     return redirect("/login")
   }
-  return [userArts, userLikes, userMasterpiece]
+
+  return [userArts, userLikes, userMasterpieces]
 }
 
 function ArtsRoot() {
-  const [userArts, userLikes, userMasterpiece] = useLoaderData();
+  const [userArts, userLikes, userMasterpieces] = useLoaderData();
+  const [isMyPage] = useOutletContext();
+  const [artsIndex, setArtsIndex] = useState(0);
 
-  const [artsIndex, setArtsIndex] = useState(0)
+  const noMasterpiece = isMyPage?
+    <div className="art-root__no-masterpiece">
+      <div>대표 작품을 설정해보세요!</div>
+      <Link to="set_masterpiece"><YellowBtn>대표작품 설정하기</YellowBtn></Link>
+    </div> :
+    <div className="art-root__no-masterpiece">
+      대표작품이 존재하지 않습니다
+    </div>
 
-  console.log(userArts);
+  const noArts = isMyPage?
+    <div className="art-root__no-arts">
+      <p>나의 작품이 존재하지 않습니다</p>
+      <p>작품을 올리고 작가가 되어보세요!</p>
+      <Link to="upload"><YellowBtn>작품 업로드</YellowBtn></Link>
+    </div> :
+    <div className="art-root__no-arts">작품이 존재하지 않습니다</div>
 
   const artMenuData = [
     { name: '작품',
@@ -41,14 +61,16 @@ function ArtsRoot() {
         userArts?
           <div>
             <h3>대표작품</h3>
-            { userMasterpiece? userMasterpiece.map((masterpiece) =>
-              <div key={`my-page__masterpiece-${masterpiece.artSeq}`}>
-                {masterpiece.artName}
-              </div>) :
-              <div>
-                <div>대표작품이 없습니다! 대표작품을 설정해보세요~</div>
-                <Link to="set_masterpiece"><YellowBtn>대표작품 설정하기</YellowBtn></Link>
-              </div>
+            { userMasterpieces?
+                userMasterpieces.map((masterpiece) =>
+                  <ArtItemMyPage
+                    key={`my-page__arts-${masterpiece.artSeq}`}
+                    nickname={masterpiece.nickname}
+                    userSeq={masterpiece.userSeq}
+                    artThumbnail={masterpiece.artThumbnail}
+                    artName={masterpiece.artName}
+                    artSeq={masterpiece.artSeq}
+                  /> ) : noMasterpiece
             }
             <h3>전체 작품</h3>
             {
@@ -60,47 +82,31 @@ function ArtsRoot() {
                   artThumbnail={art.artThumbnail}
                   artName={art.artName}
                   artSeq={art.artSeq}
-                />)}
+                /> )}
           </div>
-          :
-          <div>작품을 올리고 작가가 되어보아요~</div> },
+          : noArts
+    },
     { name: '좋아요 목록',
       content:
-        userLikes? userLikes.map((like) =>
-          <ArtItemMyPage
-            key={`my-page__art-likes-${like.artSeq}`}
-            nickname={like.nickname}
-            userSeq={like.userSeq}
-            artThumbnail={like.artThumbnail}
-            artName={like.artName}
-            artSeq={like.artSeq}
-          />)
-          :
-          <div>좋아요한 작품이 없습니다.</div> },
+        userLikes?
+          <div className="art-root__likes">
+            { userLikes.map((like) =>
+              <ArtItemMyPage
+                key={`my-page__art-likes-${like.artSeq}`}
+                nickname={like.nickname}
+                userSeq={like.userSeq}
+                artThumbnail={like.artThumbnail}
+                artName={like.artName}
+                artSeq={like.artSeq}
+              />) }
+          </div>
+          : <div className="art-root__likes" >좋아요한 작품이 없습니다.</div>
+    }
   ]
 
   return (
     <div>
-      <div>
-        <TabMenu>
-          {
-            artMenuData.map((tab, idx) =>
-              <li
-                className={idx === artsIndex ? "submenu focused" : "submenu" }
-                onClick={ () => setArtsIndex(idx) } key={`my-page__art-${idx}`}
-              >
-                {tab.name}
-              </li>
-            )
-          }
-        </TabMenu>
-        <TabContent>
-          {
-            artMenuData[artsIndex].content
-          }
-        </TabContent>
-      </div>
-
+      <TabMenuComponent menuData={artMenuData} index={artsIndex} setIndex={setArtsIndex} />
     </div>
   );
 }
