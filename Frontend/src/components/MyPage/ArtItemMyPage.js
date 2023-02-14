93ebd94838393b5340d12aecb36d6c5d5fdab5a0
gyur1kim <qri1114@gmail.com>
2023-02-14 16:33:21 +0900

    :lipstick: [FE] 마이페이지 art 수정

---
+++ Frontend/src/components/MyPage/ArtItemMyPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import { getArtThumbnail } from "../commons/imageModule";
+import '../commons/ArtComponent.css'
+
+function ArtItemMyPage({nickname, userSeq, artThumbnail, artName, artSeq}) {
+
+  return (
+    <div className="art-item__container">
+
+      <Link to={`/${nickname}/${artSeq}`} className="art-img__container">
+        <div className="art-thumbnail" style={{backgroundImage : `url("${getArtThumbnail(artThumbnail, userSeq)}")`}} />
+        <div className="hidden__gradient">
+          <div>{artName}</div>
+        </div>
+      </Link>
+
+    </div>
+  );
+}
+
+export default ArtItemMyPage;
