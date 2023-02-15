6fde51bc3c6a2bffff96a579e45633acac3f1ac1
박주희 <dam2.dev@gmail.com>
2023-02-15 15:59:14 +0900

    :memo: [FE] front 브랜치에 들어간 Backend 폴더 삭제

--- Backend/src/main/java/com/ssafy/banana/db/entity/enums/AuctionStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.banana.db.entity.enums;
-
-public enum AuctionStatus {
-	INIT, ONGOING, PAID, SUCCESS, FAILED
-}
--- Backend/src/main/java/com/ssafy/banana/dto/enums/NotificationType.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.banana.dto.enums;
-
-public enum NotificationType {
-	NOTICE, CURATION
-}
